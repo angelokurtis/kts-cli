1d1c7c4153ac6999e63a4ffa5a9264f2fad1c7c1
Tiago Angelo <kurtis.angelo@gmail.com>
2020-07-11 12:18:25 -0300

    Download pod container logs
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- cmd/kubernetes/pods/logs.go
+++ cmd/kubernetes/pods/logs.go
@@ -1,9 +1,7 @@
 package pods
 
 import (
-	"fmt"
 	"github.com/angelokurtis/kts-cli/cmd/common"
-	"github.com/angelokurtis/kts-cli/internal/color"
 	"github.com/angelokurtis/kts-cli/pkg/app/kubectl"
 	"github.com/spf13/cobra"
 )
@@ -18,16 +16,9 @@ func logs(cmd *cobra.Command, args []string) {
 		common.Exit(err)
 	}
 	for _, pod := range selects {
-		pn := pod.Metadata.Name
-		ns := pod.Metadata.Namespace
-
-		if len(pod.Spec.Containers) == 1 {
-			fmt.Printf(color.Notice, fmt.Sprintf("kubectl logs %s -n %s > ./%s.log\n", pn, ns, pn))
-			continue
-		}
-		for _, container := range pod.Spec.Containers {
-			cn := container.Name
-			fmt.Printf(color.Notice, fmt.Sprintf("kubectl logs %s -c %s -n %s> ./%s.%s.log\n", pn, cn, ns, pn, cn))
+		err := kubectl.SaveLogs(pod)
+		if err != nil {
+			common.Exit(err)
 		}
 	}
 }
