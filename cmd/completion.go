7d0bee748f8f06ef3ad0c3727bef88358510ed8c
Tiago Angelo <kurtis.angelo@gmail.com>
2020-07-24 16:32:27 -0300

    Code-complete generation
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- cmd/completion.go
+++ cmd/completion.go
@@ -1,26 +1,57 @@
 package cmd
 
 import (
-	"github.com/angelokurtis/kts-cli/cmd/common"
 	"github.com/spf13/cobra"
 	"os"
 )
 
 var completion = &cobra.Command{
-	Use:   "completion",
-	Short: "Generates bash completion scripts",
-	Long: `To load completion run
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
 
-. <(kts completion)
+Bash:
 
-To configure your bash shell to load completions for each session add to your bashrc
+$ source <(kts completion bash)
 
-# ~/.bashrc or ~/.profile
-. <(kts completion)
+# To load completions for each session, execute once:
+Linux:
+  $ kts completion bash > /etc/bash_completion.d/kts
+MacOS:
+  $ kts completion bash > /usr/local/etc/bash_completion.d/kts
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ kts completion zsh > "${fpath[1]}/_kts"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ kts completion fish | source
+
+# To load completions for each session, execute once:
+$ kts completion fish > ~/.config/fish/completions/kts.fish
 `,
-	Run: func(_ *cobra.Command, _ []string) {
-		if err := cmd.GenBashCompletion(os.Stdout); err != nil {
-			common.Exit(err)
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
 		}
 	},
 }
