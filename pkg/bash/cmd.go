4164a79ce63686b35918bfb98abdf723129110af
Tiago Angelo <kurtis.angelo@gmail.com>
2020-08-26 15:37:39 -0300

    Fix error handling

--- pkg/bash/cmd.go
+++ pkg/bash/cmd.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gookit/color"
 	"github.com/pkg/errors"
 	"os/exec"
+	"strings"
 )
 
 func RunAndLogRead(cmd string) (out []byte, err error) {
@@ -19,7 +20,11 @@ func RunAndLogWrite(cmd string) (out []byte, err error) {
 func Run(cmd string) ([]byte, error) {
 	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to execute command: %s", cmd)
+		if eerr, ok := err.(*exec.ExitError); ok {
+			msg := strings.TrimSpace(string(eerr.Stderr))
+			return nil, errors.Wrapf(eerr, "'%s' execution error: %s", cmd, msg)
+		}
+		return nil, errors.Wrapf(err, "'%s' execution error", cmd)
 	}
 	return out, nil
 }
