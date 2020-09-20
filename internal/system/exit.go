2e9bddbd63e1d8537eada516d0af50e26e13cfac
Tiago Angelo <kurtis.angelo@gmail.com>
2020-09-20 16:12:23 -0300

    Exits gracefully

---
+++ internal/system/exit.go
@@ -0,0 +1,22 @@
+package system
+
+import (
+	"github.com/gookit/color"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+)
+
+func Exit(err error) {
+	if err.Error() == "interrupt" {
+		os.Exit(1)
+	} else if _, ok := err.(stackTracer); ok {
+		log.Fatal(color.Danger.Sprintf("%+v", err))
+	} else {
+		log.Fatal(color.Danger.Sprint(err.Error()))
+	}
+}
+
+type stackTracer interface {
+	StackTrace() errors.StackTrace
+}
