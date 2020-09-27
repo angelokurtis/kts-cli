fd60f23303b60dacd320c70000227d356f0a9389
Tiago Angelo <kurtis.angelo@gmail.com>
2020-09-27 16:54:37 -0300

    Implementing container forwarding

--- cmd/kubernetes/containers/forwarding.go
+++ cmd/kubernetes/containers/forwarding.go
@@ -1,24 +1,34 @@
 package containers
 
 import (
+	"github.com/angelokurtis/kts-cli/internal/system"
+	"github.com/angelokurtis/kts-cli/pkg/app/kubectl"
+	"github.com/gookit/color"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
+// kube containers forwarding
 func forwarding(cmd *cobra.Command, args []string) {
-	//containers, err := kubectl.ListContainers(namespace, allNamespaces)
-	//if err != nil {
-	//	system.Exit(err)
-	//}
-	//container, err := containers.SelectOnePort()
-	//if err != nil {
-	//	system.Exit(err)
-	//}
-	//port, err := pod.SelectContainerPort()
-	//if err != nil {
-	//	system.Exit(err)
-	//}
-	//n := pod.Metadata.Name
-	//ns := pod.Metadata.Namespace
-	//p := strconv.Itoa(port)
-	//color.Secondary.Println("kubectl port-forward " + n + " " + p + ":" + p + " -n " + ns)
+	containers, err := kubectl.ListContainers(namespace, allNamespaces)
+	if err != nil {
+		system.Exit(err)
+	}
+	containers = containers.FilterExposed()
+	containers, err = containers.SelectMany()
+	if err != nil {
+		system.Exit(err)
+	}
+	for _, container := range containers.Items {
+		for _, port := range container.Ports {
+			n := container.Pod
+			ns := container.Namespace
+			rp := strconv.Itoa(port.ContainerPort)
+			lp := rp
+			if port.ContainerPort == 80 {
+				lp = "8000"
+			}
+			color.Secondary.Println("kubectl port-forward " + n + " " + lp + ":" + rp + " -n " + ns)
+		}
+	}
 }
