ee0b1a433af84ec95cdd5ca69700e05a2946052b
Tiago Angelo <kurtis.angelo@gmail.com>
2024-03-25 09:45:28 -0300

    Add format function for code formatting

---
+++ cmd/go/format.go
@@ -0,0 +1,29 @@
+package golang
+
+import (
+	"github.com/angelokurtis/kts-cli/internal/log"
+	"github.com/angelokurtis/kts-cli/pkg/app/git"
+	"github.com/angelokurtis/kts-cli/pkg/app/golangci_lint"
+	"github.com/spf13/cobra"
+)
+
+func format(cmd *cobra.Command, args []string) {
+	result, err := golangci_lint.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := git.ShowDiffFiles("origin/HEAD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err = result.FilterByFiles(files)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = result.PrettyPrint(); err != nil {
+		log.Fatal(err)
+	}
+}
