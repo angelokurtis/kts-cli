0c90627bca99de84d6e970de6b971b93805ad310
Tiago Angelo <kurtis.angelo@gmail.com>
2024-05-21 17:22:09 -0300

    Refactor image deletion function to use context and logging

--- cmd/docker/images/delete.go
+++ cmd/docker/images/delete.go
@@ -1,6 +1,43 @@
 package images
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"log/slog"
+	"sort"
+
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+)
 
 func del(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	dieOnErr(err)
+	defer apiClient.Close()
+
+	rawSummaries, err := apiClient.ImageList(ctx, image.ListOptions{})
+	dieOnErr(err)
+
+	sort.Slice(rawSummaries, func(i, j int) bool {
+		t1 := rawSummaries[i].Size
+		t2 := rawSummaries[j].Size
+
+		return t1 < t2
+	})
+
+	summaries := wrapImageSummaries(rawSummaries)
+	summaries, err = summaries.Select()
+	dieOnErr(err)
+
+	for _, summary := range summaries.wrapped {
+		_, err = apiClient.ImageRemove(ctx, summary.wrapped.ID, image.RemoveOptions{Force: true, PruneChildren: true})
+		if err != nil {
+			slog.WarnContext(ctx, "Failed to remove image", slog.String("image-id", summary.wrapped.ID), slog.String("error", err.Error()))
+			continue
+		}
+
+		slog.InfoContext(ctx, "Removed image", slog.String("image-id", summary.wrapped.ID))
+	}
 }
