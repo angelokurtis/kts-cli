ef6da7adb41f64af2357e7f7b490bfdab8a7c475
Tiago Angelo <kurtis.angelo@gmail.com>
2024-05-23 13:32:13 -0300

    Add logger default setup function

---
+++ internal/logger/default.go
@@ -0,0 +1,32 @@
+package logger
+
+import (
+	"github.com/angelokurtis/kts-cli/internal/otel"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+// SetUp initializes the logger with predefined settings and returns the logger instance.
+func SetUp() *slog.Logger {
+	// Create a new handler with tint colorized output
+	handler := tint.NewHandler(os.Stderr, &tint.Options{
+		AddSource:  true,
+		Level:      slog.LevelDebug,
+		TimeFormat: time.Kitchen,
+	})
+
+	// Wrap the tint handler with otel handler if otel integration is enabled (optional)
+	handler = otel.NewLogHandler(handler)
+
+	// Create a new slog logger instance with the formatted handler
+	l := slog.New(handler)
+
+	// Set the newly created logger as the default logger for the application
+	slog.SetDefault(l)
+
+	// Return the logger instance for potential further configuration
+	return l
+}
