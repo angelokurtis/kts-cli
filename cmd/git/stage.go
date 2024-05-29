5231f4b4535350606223caeb8246099c99be1491
Tiago Angelo <kurtis.angelo@gmail.com>
2024-05-29 15:55:25 -0300

    Refactor error logging to use context and tint package

--- cmd/git/stage.go
+++ cmd/git/stage.go
@@ -1,8 +1,10 @@
 package git
 
 import (
-	log "log/slog"
+	"context"
+	"log/slog"
 
+	"github.com/lmittmann/tint"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 
@@ -10,17 +12,19 @@ import (
 )
 
 func stage(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+
 	// Fetch uncommitted files
 	files, err := git.UncommittedFiles()
 	if err != nil {
-		log.Error(err.Error())
+		slog.ErrorContext(ctx, "Failed to fetch uncommitted files", tint.Err(err))
 		return
 	}
 
 	// Select files from the uncommitted files list
 	selectedFiles, err := files.SelectFiles()
 	if err != nil {
-		log.Error(err.Error())
+		slog.ErrorContext(ctx, "Failed to select files from uncommitted files list", tint.Err(err))
 		return
 	}
 
@@ -29,13 +33,13 @@ func stage(cmd *cobra.Command, args []string) {
 
 	// Stage selected but unstaged files
 	if err = stageSelectedFiles(selectedFiles); err != nil {
-		log.Error(err.Error())
+		slog.ErrorContext(ctx, "Failed to stage selected files", tint.Err(err))
 		return
 	}
 
 	// Unstage the files that were not selected but are currently staged
 	if err = unstageUnselectedFiles(unselectedFiles); err != nil {
-		log.Error(err.Error())
+		slog.ErrorContext(ctx, "Failed to unstage unselected files", tint.Err(err))
 		return
 	}
 }
