bfd2133d2a865d5dc4a5ca2826085138c3e5577c
Tiago Angelo <kurtis.angelo@gmail.com>
2024-06-18 16:55:22 -0300

    Add function to convert number to ordinal words

---
+++ internal/ordinal/ordinal.go
@@ -0,0 +1,73 @@
+package ordinal
+
+import (
+	"errors"
+	"strings"
+)
+
+func convertNumberToWords(number uint) string {
+	underTwenty := []string{
+		"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+		"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen",
+	}
+	tens := []string{"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}
+	var words []string
+
+	if number == 0 {
+		return "Zero"
+	}
+
+	if number >= 1000 {
+		thousands := number / 1000
+		words = append(words, convertNumberToWords(thousands), "Thousand")
+		number %= 1000
+	}
+
+	if number >= 100 {
+		hundreds := number / 100
+		words = append(words, convertNumberToWords(hundreds), "Hundred")
+		number %= 100
+	}
+
+	if number >= 20 {
+		t := number / 10
+		words = append(words, tens[t-2])
+		number %= 10
+	}
+
+	if number > 0 && number < 20 {
+		words = append(words, underTwenty[number-1])
+	}
+
+	return strings.Join(words, " ")
+}
+
+func FromNumber(number uint) (string, error) {
+	if number < 1 {
+		return "", errors.New("input must be a positive integer")
+	}
+
+	words := convertNumberToWords(number)
+
+	// Apply ordinal suffix adjustments
+	switch {
+	case strings.HasSuffix(words, "One"):
+		words = strings.TrimSuffix(words, "One") + "First"
+	case strings.HasSuffix(words, "Two"):
+		words = strings.TrimSuffix(words, "Two") + "Second"
+	case strings.HasSuffix(words, "Three"):
+		words = strings.TrimSuffix(words, "Three") + "Third"
+	case strings.HasSuffix(words, "Five"):
+		words = strings.TrimSuffix(words, "Five") + "Fifth"
+	case strings.HasSuffix(words, "Eight"):
+		words = strings.TrimSuffix(words, "Eight") + "Eighth"
+	case strings.HasSuffix(words, "Nine"):
+		words = strings.TrimSuffix(words, "Nine") + "Ninth"
+	case strings.HasSuffix(words, "Twelve"):
+		words = strings.TrimSuffix(words, "Twelve") + "Twelfth"
+	default:
+		words += "th"
+	}
+
+	return words, nil
+}
---
+++ internal/ordinal/ordinal_test.go
@@ -0,0 +1,32 @@
+package ordinal_test
+
+import (
+	"testing"
+
+	"github.com/angelokurtis/kts-cli/internal/ordinal"
+)
+
+func TestFromInt(t *testing.T) {
+	tests := []struct {
+		name   string
+		number uint
+		want   string
+	}{
+		{"Test for 1", 1, "First"},
+		{"Test for 22", 22, "Twenty-Second"},
+		{"Test for 45", 45, "Forty-Fifth"},
+		{"Test for 100", 100, "One Hundredth"},
+		{"Test for 101", 101, "One Hundred First"},
+		{"Test for 113", 113, "One Hundred Thirteenth"},
+		{"Test for 122", 122, "One Hundred Twenty-Second"},
+		{"Test for 1000", 1000, "One Thousandth"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := ordinal.FromNumber(tt.number); got != tt.want {
+				t.Errorf("ordinal(%d) = %s; want %s", tt.number, got, tt.want)
+			}
+		})
+	}
+}
