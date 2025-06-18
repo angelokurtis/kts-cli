16142ee97c294dcafed0b215b18cca175397d785
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-18 13:16:52 -0300

    Add error handling wrapper for cobra commands

---
+++ cmd/displays/error_handler.go
@@ -0,0 +1,20 @@
+package displays
+
+import (
+	log "log/slog"
+
+	"github.com/spf13/cobra"
+)
+
+type commandFunc func(cmd *cobra.Command, args []string) error
+
+type simpleCommandFunc func(cmd *cobra.Command, args []string)
+
+func wrapWithErrorHandler(fn commandFunc) simpleCommandFunc {
+	return func(cmd *cobra.Command, args []string) {
+		if err := fn(cmd, args); err != nil {
+			log.Error(err.Error())
+			return
+		}
+	}
+}
